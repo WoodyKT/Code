99aab07011cb8e10d5ecc5fa0382f7a86dd591d3
Woody Taylor <woodyktaylor@icloud.com>
2025-08-22 10:58:46 +0100

    added buttoms

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import sys
 # Flask setup
 app = Flask(__name__)
 simulated = False
+displayOption = 1
 
 def UpdateSensorFile():
     dataControl.WriteData(simulated)
@@ -24,12 +25,38 @@ schedule.add_job(UpdateSensorFile, 'interval', seconds=1)
 
 @app.route("/")
 def display():
-    return render_template("display.html")
+    return render_template(f"display{displayOption}.html")
 
 # Screenshot + Inky update
-PI_IP = "192.168.137.134"  # Your Pi's IP on network
+PI_IP = "192.168.137.134"  
 URL = f"http://{PI_IP}:5000"
 OUTPUT_PATH = "/home/woody/Code/screenshot.png"
+
+# Buttons
+BUTTONS = [5, 6, 16, 24] # 16 and 24 are the bottom 2
+LABELS = ["A", "B", "C", "D"]
+INPUT = gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
+chip = gpiodevice.find_chip_by_platform()
+
+# Build our config for each pin/line we want to use
+OFFSETS = [chip.line_offset_from_id(id) for id in BUTTONS]
+line_config = dict.fromkeys(OFFSETS, INPUT)
+
+# Request the lines, *whew*
+request = chip.request_lines(consumer="inky7-buttons", config=line_config)
+
+
+# "handle_button" will be called every time a button is pressed
+# It receives one argument: the associated gpiod event object.
+def handle_button(event):
+    index = OFFSETS.index(event.line_offset)
+    gpio_number = BUTTONS[index]
+    label = LABELS[index]
+    if label == "C":
+        displayOption=3-displayOption
+    elif label == "D":
+        print("debug")
+    
 
 async def take_screenshot():
     # Wait until server responds
@@ -94,3 +121,7 @@ if __name__ == "__main__":
 
     # Run the async capture loop in the main thread event loop
     asyncio.run(capture_loop())
+    
+while True:
+    for event in request.read_edge_events():
+        handle_button(event)
