3c4c8e3af71e25d38467975b5298de8aa2580455
Woody Taylor <woodyktaylor@icloud.com>
2025-09-18 17:30:07 +0100

    fixed write

--- smartPotDisplay.py
+++ smartPotDisplay.py
@@ -2,7 +2,7 @@ class dataControls:
     def WriteData(self):
       with open("static/sensorData.txt", "w") as w:
           import random
-          output = ",".join(str(random.randint(0,1000)) for _ in self._data)
+          output = ",".join(str(random.randint(0,1000)) for _ in range(5))
           print(output)
           w.write(output)
            
