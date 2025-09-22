a8650144194437626d75d58fa2507bb60a1c2070
Woody Taylor <woodyktaylor@icloud.com>
2025-09-22 15:30:10 +0100

    custom picture added

--- setScreen.py
+++ setScreen.py
@@ -1,13 +1,16 @@
 from inky.auto import auto
 from PIL import Image
+import sys
 
-print("Updating Inky Impression display...")
-try:
-    inky_display = auto()
-    img = Image.open("screenshot.png").convert("RGB")
-    img = img.resize(inky_display.resolution)
-    inky_display.set_image(img)
-    inky_display.show()
-    print("Inky display updated.")
-except:
-    print("Error, likely no inky screen connected")
+if sys.argv ==2:
+    print("Updating Inky Impression display...")
+    print(f"Image: {sys.argv[1]}")
+    try:
+        inky_display = auto()
+        img = Image.open(sys.argv[1]).convert("RGB")
+        img = img.resize(inky_display.resolution)
+        inky_display.set_image(img)
+        inky_display.show()
+        print("Inky display updated.")
+    except:
+        print("Error, likely no inky screen connected")
